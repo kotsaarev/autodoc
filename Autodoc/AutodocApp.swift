cfc0ed5498e1cbb006d289ace9a484d8fec87104
Konstantin Kotsarev <kotsaarev@gmail.com>
2025-03-19 16:38:02 +0700

    Initial Commit

---
+++ Autodoc/AutodocApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutodocApp.swift
+//  Autodoc
+//
+//  Created by Konstantin Kotsarev on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AutodocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutodocTests/AutodocTests.swift
@@ -0,0 +1,16 @@
+//
+//  AutodocTests.swift
+//  AutodocTests
+//
+//  Created by Konstantin Kotsarev on 19.03.2025.
+//
+
+import Testing
+
+struct AutodocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
